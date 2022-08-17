48cbefdd8417ee2f9c2480d11c48fbb0507a6221
unknown <evilnapsis@gmail.com>
2022-08-17 03:42:53 -0500

    add lb v4

---
+++ core/app/model/UserData.php
@@ -0,0 +1,92 @@
+<?php
+class UserData extends Extra{
+	public static $tablename = "user";
+	public $extra_fields;
+	public $extra_fields_strings;
+
+	public function __construct(){
+		$this->extra_fields = array();
+		$this->extra_fields_strings = array();
+		$this->name = "";
+		$this->lastname = "";
+		$this->username = "";
+		$this->email = "";
+		$this->password = "";
+		$this->created_at = "NOW()";
+	}
+
+
+	public function register(){
+
+		$sql = "insert into user (".$this->getExtraFieldNames().",username,email,password,created_at) ";
+		$sql .= "value (".$this->getExtraFieldValues().",\"$this->username\",\"$this->email\",\"$this->password\",$this->created_at)";
+		return Executor::doit($sql);
+	}
+
+
+	public function add(){
+		$sql = "insert into user (name,lastname,email,password,kind,created_at) ";
+		$sql .= "value (\"$this->name\",\"$this->lastname\",\"$this->email\",\"$this->password\",$this->kind,$this->created_at)";
+		Executor::doit($sql);
+	}
+
+	public function del(){
+		$sql = "delete from ".self::$tablename." where id=$this->id";
+		Executor::doit($sql);
+	}
+
+	public static function delBy($k,$v){
+		$sql = "delete from ".self::$tablename." where $k=\"$v\"";
+		Executor::doit($sql);
+	}
+
+	public function update(){
+		$sql = "update ".self::$tablename." set name=\"$this->name\",lastname=\"$this->lastname\",lastname=\"$this->lastname\",status=\"$this->status\",kind=\"$this->kind\" where id=$this->id";
+		Executor::doit($sql);
+	}
+
+	public function update_passwd(){
+		$sql = "update ".self::$tablename." set password=\"$this->password\" where id=$this->id";
+		Executor::doit($sql);
+	}
+
+	public function updateById($k,$v){
+		$sql = "update ".self::$tablename." set $k=\"$v\" where id=$this->id";
+		Executor::doit($sql);
+	}
+
+	public static function getById($id){
+		 $sql = "select * from ".self::$tablename." where id=$id";
+		$query = Executor::doit($sql);
+		return Model::one($query[0],new UserData());
+	}
+
+	public static function getBy($k,$v){
+		$sql = "select * from ".self::$tablename." where $k=\"$v\"";
+		$query = Executor::doit($sql);
+		return Model::one($query[0],new UserData());
+	}
+
+	public static function getAll(){
+		 $sql = "select * from ".self::$tablename;
+		$query = Executor::doit($sql);
+		return Model::many($query[0],new UserData());
+	}
+
+	public static function getAllBy($k,$v){
+		 $sql = "select * from ".self::$tablename." where $k=\"$v\"";
+		$query = Executor::doit($sql);
+		return Model::many($query[0],new UserData());
+	}
+
+
+	public static function getLike($q){
+		$sql = "select * from ".self::$tablename." where name like '%$q%'";
+		$query = Executor::doit($sql);
+		return Model::many($query[0],new UserData());
+	}
+
+
+}
+
+?>
